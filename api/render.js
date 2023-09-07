a722c1d0895eb24448bc5ced6f907707aaa773f2
dashpilot <support@dashpilot.com>
2023-09-07 13:38:11 +0200

    testing

--- api/render.js
+++ api/render.js
@@ -1,5 +1,5 @@
-import Layout from "../src/layouts/layout.mjs";
-import Post from "../src/components/post.mjs";
+import Layout from "./../src/layouts/layout.mjs";
+import Post from "./../src/components/post.mjs";
 
 export const config = {
   runtime: "edge",
